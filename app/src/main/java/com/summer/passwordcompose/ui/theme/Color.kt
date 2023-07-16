623499c22a7368880872e70cf40fb98bbb9eb563
kb-shub <54928767+IMshub10@users.noreply.github.com>
2023-07-16 23:39:15 +0530

    Color.kt app colors added

--- app/src/main/java/com/summer/passwordcompose/ui/theme/Color.kt
+++ app/src/main/java/com/summer/passwordcompose/ui/theme/Color.kt
@@ -2,10 +2,58 @@ package com.summer.passwordcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFD0bcff)
+val PurpleGrey80 = Color(0xFFccc2dc)
+val Pink80 = Color(0xFFefb8c8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF7d5260)
+
+val BlackDark = Color(0xFF000000)
+val WhiteDark = Color(0xFFffffff)
+
+val Black = Color(0xFF1e1e1e)
+val White = Color(0xFFdedede)
+
+val LightBlack = Color(0xFF2c2c2c)
+val LightWhite = Color(0xFFcccccc)
+
+val BlueDark = Color(0xFF008aff)
+val BlueLight = Color(0xFF2d9cdb)
+
+val HyperLink = Color(0xFF36a6ff)
+
+val BlueGreyDark = Color(0xFF455a64)
+val BlueGreyLight = Color(0xFFb0bec5)
+
+val OrangeDark = Color(0xFFff9f0A)
+val OrangeLight = Color(0xFFff9500)
+
+val GreenDark = Color(0xFF30d158)
+val GreenLight = Color(0xFF34c759)
+
+val RedDark = Color(0xFFff453a)
+val RedLight = Color(0xFFff3b30)
+
+val Error = Color(0xFFF27474)
+
+val GreyDarkest = Color(0xFF2c2c2e)
+val GreyDarker = Color(0xFF3a3a3c)
+val GreyDark = Color(0xFF48484a)
+val Grey = Color(0xFF8e8e93)
+val GreyLight = Color(0xFFc7c7cc)
+val GreyLighter = Color(0xFFd1d1d5)
+val GreyLightest = Color(0xFFe5e5ea)
+
+val Transparent = Color(0x00000000)
+val Transclusent30 = Color(0x4D2c2c2e)
+val Transclusent60 = Color(0x992c2c2e)
+
+val Opaque30 = Color(0x4debebf5)
+val Opaque60 = Color(0x99ebebf5)
+
+val SeparatorLight = Color(0x1f000000)
+val SeparatorDark = Color(0x1fffffff)
+
+val Charcoal = Color(0xFF333333)
